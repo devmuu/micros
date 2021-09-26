44314f37caa7cf929c8e1f5298e31f0d4b0dff58
Murilo Ximenes <m.andrade@alu.ufc.br>
2021-09-25 21:05:10 -0300

    adjust gitignore

--- projects/proj_05_tmr0_rele.X/main.h
+++ projects/proj_05_tmr0_rele.X/main.h
@@ -2,7 +2,7 @@
  * PIC16F877A Samples
  * Sample 05: Timer0 and Relay (with interrupt)
  * Author: David Nascimento Coelho
- * Last Update: 2018/05/11
+ * Last Update: 2021/09/25
  */
 
 #ifndef GLOBALS_H
